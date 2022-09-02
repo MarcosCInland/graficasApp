85e37d59cff983116088d87292d2d552a9eaaf5c
MarcosCInland <marcos.andres@inlandlogistics.co>
2022-09-01 23:29:02 -0600

    grafica barra component

--- src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-grafica-barra',
@@ -8,9 +10,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraficaBarraComponent implements OnInit {
 
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+  @Input() title !: number;
+  @Input() barChartData!: ChartData<'bar'>
+  @Input() horizontal: boolean = false;
+
+  /*public barChartData: ChartData<'bar'> = {
+    labels: [ '2016', '2017', '2018', '2019', '2020', '2021', '2022' ],
+    datasets: [
+      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A', backgroundColor: '#ED5F76' },
+      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B' },
+      { data: [ 18, 50, 40, 49, 36, 45, 70 ], label: 'Series C' }
+    ]
+  };*/
+
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    // We use these empty structures as placeholders for dynamic theming.
+    scales: {
+      x: {},
+      y: {
+        min: 10
+      }
+    },
+    plugins: {
+      legend: {
+        display: true,
+      },
+      /*datalabels: {
+        anchor: 'end',
+        align: 'end'
+      }*/
+    }
+  };
+  public barChartType: ChartType = 'bar';
+
   constructor() { }
 
   ngOnInit(): void {
+    if (this.horizontal) {
+      this.barChartType = 'pie'
+    }
+  }
+
+  // events
+  public chartClicked({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
+    console.log(event, active);
+  }
+
+  randomize() {
+    
   }
 
 }
